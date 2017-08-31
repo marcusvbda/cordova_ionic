a09c4691a9692546a1e9fb21d58221da07b5e56d
Marcus Vinicius Bassalobre de Assis <marcusv.bda@icloud.com>
2017-08-31 10:48:37 -0300

    correçãozinha

--- src/providers/service-provider.ts
+++ src/providers/service-provider.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response, ResponseOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+// import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 
